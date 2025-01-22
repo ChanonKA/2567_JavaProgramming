97b96c7aee40ed6d52fb2dc71525681dc01e4bd8
ChanonKA <yorizero2541@gmail.com>
2025-01-22 11:25:38 +0700

    Labsheet 06 Homworks

--- Labsheet06/src/LoanCalculator.java
+++ Labsheet06/src/LoanCalculator.java
@@ -1,4 +1,34 @@
 
 public class LoanCalculator {
+	 private String pdName;
+	    private double pdPrice;
+	    private double pdInterestRate;
+	    private int pdYear;
 
+
+
+	    public void setLoanDetails(String productName, double price, double interestRate, int years) {
+	        pdName = productName;
+	        pdPrice = price;
+	        pdInterestRate = interestRate;
+	        pdYear = years;
+	    }
+
+	    public double calculateMonthlyPayment() {
+
+	        return (pdPrice * ( (pdInterestRate / 100) / 12) ) * 
+	                Math.pow(1 + ( (pdInterestRate / 100) / 12) , pdYear * 12 ) / 
+	               (Math.pow(1 + ( (pdInterestRate / 100) / 12), pdYear * 12) - (1));
+	    }
+
+	    public void displayLoanDetails() {
+	        System.out.println("Product: " + pdName);
+	        System.out.println("Price: " + pdPrice);
+	        System.out.println("Interest Rate: " + pdInterestRate + "%");
+	        System.out.println("Loan Period: " + pdYear + " years");
+	        System.out.println("Monthly Payment: " + String.format("%.2f", calculateMonthlyPayment()));
+	    }
 }
+
+
+
