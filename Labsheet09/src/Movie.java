8b88be1ae51d1143a531b1692cbd2b50c9830608
ChanonKA <yorizero2541@gmail.com>
2025-02-05 11:24:55 +0700

    Labsheet09 Practice

---
+++ Labsheet09/src/Movie.java
@@ -0,0 +1,27 @@
+
+public class Movie {
+	private String id;
+	private String name;
+	private Director director;
+	
+	Movie(String id,String name,Director director){
+		this.id =id;
+		this.name=name;
+		this.director=director;
+		
+	}
+	Movie(){
+	  this(null,null,null);
+	}
+	public String getId() {
+		return this.id;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public Director getDirector() {
+		return this.director;
+	}
+	public 
+
+}
