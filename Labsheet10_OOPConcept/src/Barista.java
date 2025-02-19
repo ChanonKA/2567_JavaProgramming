cf076c4f958fa56d1496fe1946001ed5145c71da
ChanonKA <yorizero2541@gmail.com>
2025-02-19 11:15:01 +0700

    Labsheet10 Practice

---
+++ Labsheet10_OOPConcept/src/Barista.java
@@ -0,0 +1,22 @@
+
+public class Barista {
+	
+	private String name;
+	private char gender;
+	
+	Barista(String name,char gender){
+		this.name=name;
+		this.gender=gender;
+		
+	}
+	Barista(){
+		
+	}
+	public String getName(String name) {
+		return this.name;
+	}
+	public String getGenderName(String gender) {
+		return this.gender;
+	}
+
+}
---
+++ Labsheet10_OOPConcept/src/Drink.java
@@ -0,0 +1,14 @@
+
+public class Drink {
+	private int type;
+	private char size;
+	
+	public Drink(int type,char size) {
+		
+	}
+	Drink(){
+		
+	}
+	
+
+}
