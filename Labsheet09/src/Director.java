c1b54948c71a134410de4161497a13ddff506c00
ChanonKA <yorizero2541@gmail.com>
2025-02-19 08:39:24 +0700

    Attendance 19/02/68

--- Labsheet09/src/Director.java
+++ Labsheet09/src/Director.java
@@ -1,4 +1,28 @@
 
 public class Director {
+	private String name;
+	private String email;
+	private char gender;
+	
+	Director (String name,String email,char gender){
+		this.name=name;
+		this.email=email;
+		this.gender=gender;
+	}
+	Director(String name,String email){
+		this(name,email,' ');
+	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getGenderName() {
+		return ' ';
+	}
+	public String toString() {
+		return getName()+"("+getEmail()+")("+getGenderName()+"%)";
+	}
 
 }
